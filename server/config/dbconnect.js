d75f7b92be641625ca84605a64467bb111ea14d3
Truelierr <hanshul.k@kalvium.community>
2024-07-22 16:29:00 +0530

    mongodb bug

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -3,12 +3,21 @@ const mongoose = require('mongoose');
 const MONGO_URI = process.env.DATABASEURI
 
 const ConnectToDB = async () => {
-    try {
-        const conn = await mongoose.connect(MONGO_URI);
-        console.log(`Connected to MongoDB`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
+    // try {
+    //     const conn = await mongoose.connect(MONGO_URI);
+    //     console.log(`Connected to MongoDB`);
+    // } catch (error) {
+    //     console.error(`Error: ${error.message}`);
+    // }
+
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      .then(() => console.log('MongoDB connected...'))
+      .catch(err => console.error('MongoDB connection error:', err));
 }
+
+
 
 module.exports = ConnectToDB
